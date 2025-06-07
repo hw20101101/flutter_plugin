a0d9721545703d1516284a966eabd097d7006a58
huangwei <hwacdx@163.com>
2025-06-07 18:28:46 +0800

    删除旧 demo

--- example/android/app/src/main/kotlin/com/example/flutter_plugin2_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_plugin2_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/flutter_plugin2.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_plugin2_platform_interface.dart';
-
-class FlutterPlugin2 {
-  Future<String?> getPlatformVersion() {
-    return FlutterPlugin2Platform.instance.getPlatformVersion();
-  }
-}
